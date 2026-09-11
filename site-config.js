a46491d518bcc83fb8248af75f3ae87997bc4433
Lucas Janoca <Lucasjanoca9@gmail.com>
2026-09-10 21:54:14 -0300

    Centraliza URL oficial do canal do YouTube

--- site-config.js
+++ site-config.js
@@ -1,1 +1,1 @@
-window.ITALO_SITE_CONFIG=Object.freeze({"lockVideoSelection":true,"lockViewCounts":true,"videoSlots":8,"courseUrl":"https://pay.kiwify.com.br/yAdCPJy"});
+window.ITALO_SITE_CONFIG=Object.freeze({"lockVideoSelection":true,"lockViewCounts":true,"videoSlots":8,"courseUrl":"https://pay.kiwify.com.br/yAdCPJy","youtubeChannelUrl":"https://www.youtube.com/channel/UC972onsIyHgDN0VC2u4W3zA"});
