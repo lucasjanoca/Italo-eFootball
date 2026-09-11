5c6edc74d0839b20940bd0992d92eecbccd13a10
Lucas Janoca <Lucasjanoca9@gmail.com>
2026-09-10 21:08:20 -0300

    Atualiza link oficial do curso para Kiwify

--- site-config.js
+++ site-config.js
@@ -1,1 +1,1 @@
-window.ITALO_SITE_CONFIG=Object.freeze({"lockVideoSelection":true,"lockViewCounts":true,"videoSlots":8,"courseUrl":"https://linktr.ee/italoefootbal"});
+window.ITALO_SITE_CONFIG=Object.freeze({"lockVideoSelection":true,"lockViewCounts":true,"videoSlots":8,"courseUrl":"https://pay.kiwify.com.br/yAdCPJy"});
